a6f6db8f157fa09fecd50191b9f8bd58207f5a0b
Ashok Kumar Pant <asokpant@gmail.com>
2025-04-03 13:53:03 +0545

    Initial setup

---
+++ README.md
@@ -0,0 +1,33 @@
+# KULLM
+
+It contains LLM use cases and api to access them. It also has demo UI.
+
+## Features
+
+1. Surname classification
+
+## Dependencies
+
+Use poetry
+
+```bash 
+poetry install
+```
+
+Add new dependency
+
+```bash
+poetry add torch
+```
+
+## Build
+
+## Run
+
+```bash
+```
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+"""
+-- Created by: Ashok Kumar Pant
+-- Email: asokpant@gmail.com
+-- Created on: 03/04/2025
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="kullm",
+    version="1.0.0",
+    description="KU LLMS for AICL434",
+    author="Ashok Kumar Pant",
+    author_email="asokpant@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+)
